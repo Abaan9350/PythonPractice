6189f1957b73eb965641739b8472190886889ba9
Abaan9350 <139568623+Abaan9350@users.noreply.github.com>
2023-12-27 20:18:33 +0300

    Basics of variables

--- main.py
+++ main.py
@@ -5,3 +5,18 @@ print("Difference kya hain phir?!")
 
 # we will learn variables now
 
+name = "Abaan"
+#print(type(name)) #This will print the datatype of our variable
+#print(name)
+
+first_name = "Abaan"
+last_name = "Sarguroh"
+
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+age = 18
+
+print("Hello, "+full_name + ", who is " +str(age) + " years old.")
+
